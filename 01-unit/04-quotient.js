012ae42e25130d042cfecf72466f79fbb0ccc0eb
haka08 <91915361+haka08@users.noreply.github.com>
2021-11-15 15:21:33 +0800

    Update (test case 5) to satisfy testcases (quotient)

--- 01-unit/04-quotient.js
+++ 01-unit/04-quotient.js
@@ -45,6 +45,9 @@ try {
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = quotient();
+  if (result !== 0) throw new Error('Expected quotient() to be 0. Received: ' + result);
+  console.log('All tests passed successfully.');
 
   console.log('All tests passed successfully.');
 
