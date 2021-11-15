e091cc53575e45a191b4f7ef0e4048c1f702d67b
haka08 <91915361+haka08@users.noreply.github.com>
2021-11-15 15:20:59 +0800

    Update (test case 4) to satisfy testcases (quotient)

--- 01-unit/04-quotient.js
+++ 01-unit/04-quotient.js
@@ -38,6 +38,8 @@ try {
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = quotient(2);
+  if (result !== 2) throw new Error('Expected quotient(2) to be 2. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 5
